84db253fa805bffc83d3f091ac70936842c8d978
Camila Martinez <camilamarb98@gmail.com>
2022-04-06 18:08:07 -0500

    create run app config

---
+++ app/app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+
+from . import cards
+from . import static_pages
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config')
+
+    register_blueprints(app)
+    return app
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(cards.routes.blueprint)
+    app.register_blueprint(static_pages.routes.blueprint)
---
+++ app/config.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
